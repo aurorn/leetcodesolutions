7a22ddab1b4f6045f5fa9d503f4ed88fe7865eb7
Aurorn <aurornispro@gmail.com>
2025-02-20 12:50:43 -0500

    Adding more solutions

--- problems/script.js
+++ problems/script.js
@@ -17,18 +17,58 @@ const createCounter = function(n) {
 // Problem 3 - Expect Function 
 const expect = function(val) {
     return {
-        toBe(val) {
-            if (val === val) {
-                return true;
-            } else (val !== val)
-                return "Not Equal";
-            
-        },
-        notToBe(val) {
-            if (val !== val) {
+        toBe: function(expected) {
+            if (val === expected) {
                 return true;
             }
+            throw new Error("Not Equal");
         },
+        notToBe: function(expected) {
+            if (val !== expected) {
+                return true;
+            }
+            throw new Error("Equal");
+        }
+    };
+}
 
+// Problem 4 - Counter Function II
+
+const createCounter2 = function(init) {
+    let currentValue = init;
+    return {
+        increment: function() {
+            return ++currentValue;
+        },
+        decrement: function() {
+            return --currentValue;
+        },
+        reset: function() {
+            currentValue = init;
+            return currentValue;
+        }
     }
+}
+
+// Problem 5 - Apply Transformation over Each Element in Array
+
+const map = function(arr, fn) {
+    const transformedArr = new Array(arr.length);
+    for (let i = 0; i < arr.length; i++) {
+        transformedArr[i] = fn(arr[i], i);
+    }
+    return transformedArr;
+};
+
+
+// Problem 6 - Filter Elements from Array
+
+const filter = function(arr, fn) {
+    const filteredArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            filteredArr.push(arr[i]);
+        }
+    }
+    return filteredArr;
 }
